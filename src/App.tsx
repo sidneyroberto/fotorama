173a4654592a3817e4aa53971629a5cae54128cd
Sidney Sousa <sidney.sousa@ifms.edu.br>
2023-08-18 15:09:47 -0400

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useRoutes } from 'react-router-dom'
+import { UserContextProvider } from './context/UserContext'
+
+import { routes } from './routes'
+
+const App = () => {
+  const elements = useRoutes(routes)
+
+  return <UserContextProvider>{elements}</UserContextProvider>
+}
+
+export default App
